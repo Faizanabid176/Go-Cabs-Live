41ce520c5d058c303aa3c441c23cfbc4fc75ca0c
faizanabid176 <faizanabid4000@gmail.com>
2023-11-02 17:27:57 +0500

    1st Commit

---
+++ lib/config.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+var schemecolor = Colors.yellow;
+var bgcolor = Color(0xfff6f6f6);
+var textboxcolor = const Color(0xfff4eef0);
+var inputfieldcolor = Color.fromARGB(255, 212, 212, 212);
+var loginicon = const Color(0xffcccccc);
+var primaycolor = const Color.fromARGB(125, 0, 255, 8);
+var darkblue = Color(0xff3d4ef4);
+
+// var configurl = 'http://192.168.18.133:40/';
+var configurl = 'https://kifkifdz-001-site2.gtempurl.com/KifKif/';
+var userurl =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8wvbPOt0gK-5yGATP1Beo7Mkk7LT1M6KZLw&usqp=CAU';
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
